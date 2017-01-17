acd5b49cc2dc54cb51ac508f958922fc8d3363b9
teom <teo.mertanen@aalto.fi>
2017-01-17 16:29:31 +0200

    create and read games functionality

--- game/models.py
+++ game/models.py
@@ -1,13 +1,22 @@
 from django.db import models
+from django.forms import ModelForm
 
 # Create your models here.
 
 class Game(models.Model):
     id = models.AutoField(primary_key=True)
+    """developer = models.ForeignKey(
+        'User',
+        on_delete=models.CASCADE,
+    )"""
     title = models.CharField(max_length=255, default="Title", unique=True)
     description = models.TextField(default='descr')
     image_url = models.URLField(blank = True)
-    quantity = models.IntegerField(default=0)
     def set_title(self, new_title):
         self.title = new_title
         self.save()
+
+class GameForm(ModelForm):
+    class Meta:
+        model = Game
+        fields = ['title', 'description', 'image_url']
--- game/views.py
+++ game/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse, Http404
 from django.shortcuts import render
-from game.models import Game
+from game.models import Game, GameForm
 
 def index(request):
     return HttpResponse("Default page when logged in. As a player, see the overview of your profile, games owned, and highscores.  ")
@@ -17,4 +17,12 @@ def gameview(request, product_id):
 def available_games(request):
     """A view of all games."""
     games = Game.objects.all()
-    return render(request, 'game/game_list.html', {'games': games})
+    new_game = Game()
+    form = GameForm(initial={'title': 'Super Django Bros.'}, instance=new_game)
+    if request.method == 'POST':
+        form = GameForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+    else:
+        form = GameForm()
+    return render(request, 'game/game_list.html', {'games': games, 'form': form})
