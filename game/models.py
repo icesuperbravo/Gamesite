5bc6d33ee91a3c5117bbf03e376091b8f32d83c4
mertant1 <teo.mertanen@aalto.fi>
2017-01-17 13:24:49 +0200

    add Game model

--- game/models.py
+++ game/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Game(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=255, default="Title", unique=True)
+    description = models.TextField(default='descr')
+    image_url = models.URLField(blank = True)
+    quantity = models.IntegerField(default=0)
+    def set_title(self, new_title):
+        self.title = new_title
+        self.save()
