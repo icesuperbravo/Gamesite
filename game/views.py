3a8285b8fd10516be41987b50ad26e51f289e130
teom <teo.mertanen@aalto.fi>
2017-01-17 15:14:43 +0200

    import Game model

--- game/views.py
+++ game/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse, Http404
 from django.shortcuts import render
-
+from game.models import Game
 
 def index(request):
     return HttpResponse("Default page when logged in. As a player, see the overview of your profile, games owned, and highscores.  ")
@@ -11,10 +11,10 @@ def about(request):
 
 def gameview(request, product_id):
     """A view of a single game."""
-    game = models.Games.objects.get(pk=product_id)
+    game = Game.objects.get(pk=product_id)
     return render(request, 'game/game_view.html', {'game': game})
 
 def available_games(request):
     """A view of all games."""
-    games = models.Games.objects.all()
+    games = Game.objects.all()
     return render(request, 'game/game_list.html', {'games': games})
