044e32638306f6138b7b41dede68b3c0401694ab
icesuperbravo <icesuperbravo@icesuperbravodeMacBook-Pro.local>
2017-02-02 17:59:51 +0200

    fix typo

--- gamesite/settings.py
+++ gamesite/settings.py
@@ -32,7 +32,7 @@ DEBUG = True
 
 INSTALLED_APPS = (
     'django.contrib.admin',
-    'django.contrib.auth',gi
+    'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
