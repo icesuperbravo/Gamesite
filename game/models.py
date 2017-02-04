ca30640bb07b5a3c28a3e4b415f8dd8d9c837a10
teom <teo.mertanen@aalto.fi>
2017-02-04 13:44:04 +0200

    remove unused enum import

--- game/models.py
+++ game/models.py
@@ -1,7 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
 from django.forms import ModelForm
-from enum import Enum
 
 from django.db.models.signals import post_save
 from django.dispatch import receiver
